35b1f831f175242caa572d8f2e063bfaa3e628d3
Michael Hommrich <mhommrich@gmail.com>
2026-01-24 18:42:40 -0500

    triggering build

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <h1>Today's Workout:</h1>
-      <p>Chest</p>
       <div className="body">
         {!inProgress &&
           <button onClick={() => setInProgress(true)}>
