949e28eba7298c2fe8e00651b66eb76989724b7c
Michael Hommrich <mhommrich@gmail.com>
2026-01-24 18:33:25 -0500

    first commit - cloning repo

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [inProgress, setInProgress] = useState(false)
+
+  return (
+    <>
+      <h1>Today's Workout:</h1>
+      <p>Chest</p>
+      <div className="body">
+        {!inProgress &&
+          <button onClick={() => setInProgress(true)}>
+            Start workout
+          </button>
+        }
+        {inProgress &&
+          <button onClick={() => setInProgress(false)}>
+            End workout
+          </button>
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
